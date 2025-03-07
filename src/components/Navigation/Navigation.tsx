31ce353ef978994d770d0f4bf643e50bde27ddc7
Edelwei555 <dima.vasilenko.27@gmail.com>
2025-03-07 17:28:24 +0200

    Оновлено навігацію: видалено дублюючі елементи та синю шапку

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,51 +2,37 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useAuth } from '../../contexts/AuthContext';
-import { AppBar, Toolbar, Typography, Button, IconButton, Menu, MenuItem } from '@mui/material';
-import { AccountCircle, Timer, Group, Language } from '@mui/icons-material';
+import { Button, IconButton, Menu, MenuItem } from '@mui/material';
+import { Timer, Group, Language } from '@mui/icons-material';
 import './Navigation.css';
 
 const Navigation: React.FC = () => {
   const { t } = useTranslation();
   const location = useLocation();
   const { currentUser, signOut } = useAuth();
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [languageAnchor, setLanguageAnchor] = React.useState<null | HTMLElement>(null);
-
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
 
   const handleLanguageMenu = (event: React.MouseEvent<HTMLElement>) => {
     setLanguageAnchor(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
   };
 
   const handleLanguageClose = () => {
     setLanguageAnchor(null);
   };
 
-  const handleProfile = () => {
-    handleClose();
-  };
-
   const handleLogout = async () => {
-    handleClose();
     await signOut();
   };
 
   if (!currentUser) return null;
 
   return (
-    <AppBar position="static" className="navigation">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Work Tracking
-        </Typography>
+    <div className="navigation">
+      <div className="nav-left">
+        <h1 className="logo">Work Tracking</h1>
+      </div>
 
+      <div className="nav-center">
         <Button
           color="inherit"
           component={Link}
@@ -66,33 +52,15 @@ const Navigation: React.FC = () => {
         >
           {t('nav.teams')}
         </Button>
+      </div>
 
+      <div className="nav-right">
         <IconButton
           onClick={handleLanguageMenu}
           color="inherit"
         >
           <Language />
         </IconButton>
-
-        <IconButton
-          onClick={handleMenu}
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-
-        <Menu
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleProfile} component={Link} to="/profile">
-            {t('nav.profile')}
-          </MenuItem>
-          <MenuItem onClick={handleLogout}>
-            {t('nav.logout')}
-          </MenuItem>
-        </Menu>
 
         <Menu
           anchorEl={languageAnchor}
@@ -106,8 +74,21 @@ const Navigation: React.FC = () => {
             English
           </MenuItem>
         </Menu>
-      </Toolbar>
-    </AppBar>
+
+        <div className="user-info">
+          <Link to="/profile" className="email-link">
+            {currentUser.email}
+          </Link>
+          <Button
+            color="inherit"
+            onClick={handleLogout}
+            className="logout-button"
+          >
+            {t('nav.logout')}
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 };
 
