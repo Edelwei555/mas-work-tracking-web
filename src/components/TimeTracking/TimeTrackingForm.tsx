d3ff7927caf8b06e0ee85db33921b348c70f7681
Edelwei555 <dima.vasilenko.27@gmail.com>
2025-03-30 21:57:46 +0300

    fix: update select placeholders in time tracking form

---
+++ src/components/TimeTracking/TimeTrackingForm.tsx
@@ -0,0 +1,31 @@
+export const TimeTrackingForm = () => {
+  return (
+    <div className="time-tracking-form">
+      <div className="form-group">
+        <h2>Види робіт</h2>
+        <select 
+          className="form-select"
+          placeholder="Вибрати вид роботи"
+          defaultValue=""
+        >
+          <option value="" disabled>Вибрати вид роботи</option>
+          {/* ... options ... */}
+        </select>
+      </div>
+
+      <div className="form-group">
+        <h2>Локації</h2>
+        <select 
+          className="form-select"
+          placeholder="Вибрати локацію"
+          defaultValue=""
+        >
+          <option value="" disabled>Вибрати локацію</option>
+          {/* ... options ... */}
+        </select>
+      </div>
+
+      {/* ... rest of the form ... */}
+    </div>
+  );
+}; 
