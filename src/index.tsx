0c97c8ba4ad11e12a530dc3913fde8792aeee70c
Edelwei555 <dima.vasilenko.27@gmail.com>
2025-04-06 00:02:50 +0300

    fix: clean up index.tsx and remove duplicate imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { AppProviders } from './providers/AppProviders';
-import App from './App';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from './store/store';
 import { AuthProvider } from './contexts/AuthContext';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './i18n';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
